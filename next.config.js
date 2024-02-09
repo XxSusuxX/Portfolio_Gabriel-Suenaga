20b7c11ac14f9f82d23c238f9937f7abb16c0363
Gabriel <130417250+XxSusuxX@users.noreply.github.com>
2024-02-09 15:27:32 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'instagram.ftow5-1.fna.fbcdn.net'
+                hostname: 'scontent.cdninstagram.com'
             }
         ]
     },
